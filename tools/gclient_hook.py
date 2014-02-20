ce90be1f2bf3375eee1e865e31e79ca03bc395d1
Mark Chandler <mark@moddb.com>
2014-02-19 18:00:33 -0800

    cef_gyp_gclient_hook.patch from http://github.com/lindenlab/desura-app

--- tools/gclient_hook.py
+++ tools/gclient_hook.py
@@ -54,5 +54,37 @@ RunAction(cef_dir, patcher);
 
 print "\nGenerating CEF project files..."
 os.environ['CEF_DIRECTORY'] = os.path.basename(cef_dir);
-gyper = [ 'python', 'tools/gyp_cef', 'cef.gyp', '-I', 'cef.gypi' ]
+
+if sys.platform.startswith("win"):
+	gyper = [ 'python', 'tools/gyp_cef', 'cef.gyp',
+		'-I', 'cef.gypi',
+		'-Dwerror=',
+		'-Dchromium_win_pch=1',
+		'-Dmsvs_multi_core_compile=1',
+		'-Dproprietary_codecs=1',
+		'-Dffmpeg_branding=Desura']
+else:
+	gyper = [ 'python', 'tools/gyp_cef', 'cef.gyp',
+		'-I', 'cef.gypi',
+		'-Dwerror=',
+		'-Duse_cups=0',
+		'-Duse_gconf=0',
+		'-Duse_gnome_keyring=0',
+		'-Duse_kerberos=0',
+		'-Dproprietary_codecs=1',
+		'-Dffmpeg_branding=Desura',
+		'-Duse_system_bzip2=1',
+		'-Duse_system_flac=1',
+		'-Duse_system_icu=0',
+		'-Duse_system_libevent=1',
+		'-Duse_system_libjpeg=1',
+		'-Duse_system_libpng=1',
+		'-Duse_system_libwebp=0',
+		'-Duse_system_libxml=1',
+		'-Duse_system_speex=1',
+		'-Duse_system_v8=0',
+		'-Duse_system_xdg_utils=1',
+		'-Duse_system_yasm=1',
+		'-Duse_system_zlib=1']
+
 RunAction(cef_dir, gyper);
